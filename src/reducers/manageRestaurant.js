f13f870ef85e6c970a9a63cf79eacde9d7f1a181
Robert Snyder <r.snyder49@gmail.com>
2019-01-19 23:42:24 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -3,20 +3,21 @@ export const cuidFn = cuid;
 
 export default function manageRestaurants(state = {restaurants: [], reviews: []}, action) {
   
+  switch (action.type) {
   case 'ADD_RESTAURANT':
             return {...state, restaurants: [...state.restaurants, {id: cuidFn(), text: action.text}] }
-        case 'DELETE_RESTAURANT':
+  case 'DELETE_RESTAURANT':
             return {...state, restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)};
-        case 'ADD_REVIEW':
+  case 'ADD_REVIEW':
             return {...state, reviews: [...state.reviews, {
                 id: cuidFn(),
                 text: action.review.text,
                 restaurantId: action.review.restaurantId 
                 }]
             }
-        case 'DELETE_REVIEW':
+  case 'DELETE_REVIEW':
             return {...state, reviews: state.reviews.filter(review => review.id !== action.id)}
-        default:
+  default:
             return state;
     }
 } 
