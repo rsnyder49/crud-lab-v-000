29d21c47f4d4c36aab3bbcae33c337406d7b0623
Robert Snyder <r.snyder49@gmail.com>
2019-01-19 23:14:00 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import ReviewsContainer from '../../containers/ReviewsContainer'
 
 class Restaurant extends Component {
-
-
+  
+  handleOnClick() {
+    this.props.deleteRestaurant(this.props.restaurant.id);
+  }
+  
   render() {
     const { restaurant } = this.props;
 
