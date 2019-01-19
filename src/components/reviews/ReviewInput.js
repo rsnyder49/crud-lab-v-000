e1dfc8e0552eb6718f78259d91a43943246534d8
Robert Snyder <r.snyder49@gmail.com>
2019-01-19 23:33:30 +0000

    Automatically backed up by Learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  
+  state = {text: ''}
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId });
+    this.setState({text: ''});
+  }
+  
   render() {
     return (
       <div>
