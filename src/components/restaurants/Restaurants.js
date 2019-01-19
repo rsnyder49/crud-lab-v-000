99fce29212a8627cbbe1123c5322fea8654d42c5
Robert Snyder <r.snyder49@gmail.com>
2019-01-19 23:20:28 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -3,6 +3,16 @@ import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+    const {restaurants, restaurant, deleteRestaurant } = this.props;
+    const restaurants_list = restaurants.map(restaurant => {
+      return (
+        <Restaurant
+            key={restaurant.id}
+            restaurant={restaurant}
+            deleteRestaurant={deleteRestaurant}
+        />
+      )
+    });
     return(
       <ul>
         Restaurants Component
