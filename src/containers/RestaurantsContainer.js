699809c68e832d552e8bd3f20e6ab495e15d6041
Robert Snyder <r.snyder49@gmail.com>
2019-01-19 23:38:37 +0000

    Automatically backed up by Learn

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import RestaurantInput from '../components/restaurants/RestaurantInput'
 import Restaurants from '../components/restaurants/Restaurants'
+import { connect } from 'react-redux'
 
 class RestaurantsContainer extends Component {
 
