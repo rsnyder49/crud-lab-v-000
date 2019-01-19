0caf8c07c8582fdc98a0e344c016cce33532a1b9
Robert Snyder <r.snyder49@gmail.com>
2019-01-19 23:17:02 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  
+  state = {text: ''}
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text);
+    this.setState({text: ''});
+  }
+  
   render() {
     return (
       <div>
