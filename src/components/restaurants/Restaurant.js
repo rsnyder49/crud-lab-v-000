84415194a0e1a44d2bcd95ca04cf504512a2c801
Robert Snyder <r.snyder49@gmail.com>
2019-01-19 23:15:04 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -15,7 +15,7 @@ class Restaurant extends Component {
         <li>
           {restaurant.text}
           <button onClick={() => this.handleOnClick()}> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
+          <ReviewsContainer restaurant={restaurant} restaurantId={restaurant.id}/>
         </li>
       </div>
     );
